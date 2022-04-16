33d8a1bce546add0d73cdf1010b23cc486cad680
Seiki Tokunaga <otokuna46@gmail.com>
2022-04-16 19:51:19 +0900

    Fix test

--- blog_test.go
+++ blog_test.go
@@ -5,7 +5,20 @@ import "testing"
 func TestSaveArticle(t *testing.T) {
 	blog := New()
 	blog.SaveArticle(Article{"My title", "My post body"})
-	if blog.Articles[0].Title != "Mytitle" {
-		t.Errorf("Item was not expected title")
+	testTitle := "My title"
+	if blog.Articles[0].Title != testTitle {
+		t.Errorf("Item was not expected title expect My title but actual %s", blog.Articles[0].Title)
+	}
+}
+
+func TestFetchAllArticles(t *testing.T){
+	blog := New()
+
+	blog.SaveArticle(Article{"My title", "My post body"})
+
+	articles := blog.FetchAll()
+
+	if len(articles) == 0 {
+		t.Errorf("Fetch all number of articles")
 	}
 }
